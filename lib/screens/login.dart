de4feefcb206b9fb5d60b15739853995221a411c
amwo <nuude.ad@gmail.com>
2019-03-29 15:26:23 +0900

    Add Login widget

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,11 @@
 class Login extends StatefulWidget {
     @override
+    State createState() => new LoginStates();
+}
+
+class LoginState extends State<Loign> {
+    @override
+    Widget build(BuildContext context) {
+        return Text("Login");
+    }
 }
