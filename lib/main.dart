38791e1b180816600a2cf41766a78aa4d5df20de
amwo <nuude.ad@gmail.com>
2019-04-03 14:12:19 +0900

    Update few chenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-
-// Screens
 import 'package:apps/screens/home.dart';
-
-// States Management
 import 'package:rxdart/rxdart.dart';
 
 void main() => runApp(new Home());
