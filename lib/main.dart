284d572ba65da61ec010bd0832142abd099a063f
amwo <nuude.ad@gmail.com>
2019-04-03 20:29:36 +0900

    Add configure files

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:apps/screens/home.dart';
 import 'package:rxdart/rxdart.dart';
+//import 'dart:async';
 
 void main() => runApp(new Home());
