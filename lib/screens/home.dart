181efe1e8afc2c3cddb4fa0b797def4cde9a33af
amwo <nuude.ad@gmail.com>
2019-04-03 20:29:18 +0900

    Add Firebase Connection.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-                title: 'Welcome to Flutter',
-                home: Scaffold(
-                        appBar: AppBar(
-                                title: Text('Title'),
-                        ),
-                        body: Center(
-                                child: Text('Goold'),
-                        ),
-                ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('App'),
+          ),
+          body: new TestList()),
+    );
+  }
+}
+
+class TestList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('data').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError)
+          return new Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting: return new Text('Loading...');
+          default:
+            return new ListView(
+              children: snapshot.data.documents.map((DocumentSnapshot document) {
+                return new ListTile(
+                  title: new Text(document['title']),
+                  subtitle: new Text(document['content']),
+                );
+              }).toList(),
+            );
+        }
+      },
+    );
+  }
 }
